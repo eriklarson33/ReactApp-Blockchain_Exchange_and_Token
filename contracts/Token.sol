d41a70877cd4818c1d5d0d00db878b93c8f75953
Erik Larson <eriklarson@Eriks-MacBook-Air.local>
2022-09-06 09:50:26 -0500

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,10 +8,14 @@ contract Token{
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
+    // Track Balances
+    mapping(address => uint256) public balanceOf;
+    // Send Tokens
 
     constructor(string memory _name, string memory _symbol, uint _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -8,8 +8,12 @@ const tokens = (n)=> {
 // describe() is a function provided by the hardhat test runner...
 // It's a wrapper that you add a string to describe what you're doing and then the function runs all the tests.
 describe("Token", ()=> {
-    // Declare Token variable so that it's accessible to all funcitons.
-    let token
+    // Declare Token variable so that it's accessible to all funcitons. NOTE: Can make all declarations in 1 line.
+    let token, 
+        accounts, 
+        deployer
+    // let accounts
+    // let deployer
 
     // beforeEach function executes code before each of the following test functions.
     beforeEach( async ()=> {
@@ -17,6 +21,8 @@ describe("Token", ()=> {
         // NOTE: to call await, it needs to be inside of an async (asynchronous) function.
         const Token = await ethers.getContractFactory('Token') // Gets the contract itself
         token = await Token.deploy('My Token', 'TOK', '1000000') // Gets the deployed copy of the contract
+        accounts = await ethers.getSigners() // Get an array of Test Accounts
+        deployer = accounts[0] // Get the 1st account as the deploying account.
     })
 
     describe('Deployment', ()=> {
@@ -24,7 +30,7 @@ describe("Token", ()=> {
         const symbol = 'TOK'
         const decimals = '18'
         const totalSupply = tokens('1000000')
-        
+
         // "it" is used for each test example. 1st argument is a describing string, 2nd is a function
         it("has the correct name", async()=> {
             // Read the token name.
@@ -44,6 +50,10 @@ describe("Token", ()=> {
 
         it('has corrrect total supply.', async()=> {
             expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+
+        it('assigns Total Supply to contract deployer', async()=> {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
         })
     })
 }) 
