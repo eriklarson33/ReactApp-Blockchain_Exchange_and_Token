231309ab27617e43bc26ee28afa032814e1c7698
Erik Larson <eriklarson@Eriks-MacBook-Air.local>
2022-10-07 15:04:37 -0500

    11. Make Trade Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,12 @@ contract Exchange{
     // Maps Token Address to the User Address & how many Tokens that User has.
     mapping(address => mapping(address => uint256)) public tokens;
 
+    // Trade Orders Mapping
+    mapping(uint256 => _Order) public orders;  // Use the Order Id(uint256) to look up the Order
+
+    // Have an Order Timestamp Cache
+    uint256 public orderCount;  // Represents the total # of orders that have been created.
+
     // Event to record deposit
     event Deposit(address token, address user, uint256 amount, uint256 balance);
 
@@ -20,6 +26,29 @@ contract Exchange{
         uint256 amount,
         uint256 balance
     );
+
+    // Event to record Trade Orders
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+// Using a "struct" to model the Order.
+    struct _Order {
+        // Attributes of an Order
+        uint256 id;  // Unique Identifier for the order (we'll create this ID Manually)
+        address user;  // Address of the user who made the order
+        address tokenGet; // Address of the token they receive
+        uint256 amountGet;  // Amount they receive
+        address tokenGive;  // Address of the token they give
+        uint256 amountGive;  // Amount they give
+        uint256 timestamp;  // When the order was created
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -64,6 +93,39 @@ contract Exchange{
         returns(uint256) {
             return tokens[_token][_user];
         }
+
+    // ---------------------
+    // MAKE & CANCEL ORDERS
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount + 1;
+        // Require Token Balance
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+        // Token Give (the token user wants to wants to spend) - Which Token & How Much?
+        // Token Get (the token the user wants to receive) - Which Token & How Much?
+        orders[orderCount] = _Order(
+            orderCount, // ID
+            msg.sender, // user
+            _tokenGet, // tokenGet
+            _amountGet, // amountGet
+            _tokenGive, // tokenGive
+            _amountGive, // amountGive
+            block.timestamp// timestamp (@17:02 minutes)
+        );
+
+        // Emit Event
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+    }
+
 }
 
 /*
